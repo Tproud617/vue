66251efcaa1d02f4cdeba258768b519b274d517a
Tproud617 <2665634362@qq.com>
2025-06-05 16:20:13 +0800

    无敌牛逼吊炸天

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import vue from '@vitejs/plugin-vue'
 const path = require('path')
 
 export default defineConfig({
-  // 开发模式下不使用基础URL，便于调试
-  base: '/',  // 已修改：从 '/vue/' 改为 '/'
+  base: '/vue/',  // 已修改：从 '/' 改为 '/vue/'
   plugins: [vue()],
   resolve: {
     alias: {
